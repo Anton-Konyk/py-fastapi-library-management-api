f222250b27d78db43a3942e30eb49cfe703c0b0e
Anton Konyk <antonkonyk.com>
2024-12-06 18:13:18 +0100

    create function create_book

--- crud.py
+++ crud.py
@@ -14,3 +14,16 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     db.refresh(db_author)
 
     return db_author
+
+
+def create_book(db: Session, book: schemas.BookCreate):
+    db_book = models.DBBook(
+        title=book.title,
+        summary=book.price,
+        publication_date=book.publication_date,
+        author_id=book.author_id,
+    )
+    db.add(db_book)
+    db.commit()
+    db.refresh(db_book)
+    return db_book
